728bb42a9fa939c37695a2e1041deadc151d4095
Ahmed Farag <ahmedfarag.a95@gmail.com>
2019-08-09 20:02:45 +0200

    Edited import.py to add code for creating two new tables in db.

--- import.py
+++ import.py
@@ -9,6 +9,8 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
+
+
     db.execute("CREATE TABLE books ( id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL  );")
     print("Created books table in database")
 
@@ -21,6 +23,15 @@ def main():
         print(x)
         x += 1
     print("Added info about all books")
+
+
+    db.execute("CREATE TABLE users ( id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, password VARCHAR NOT NULL );")
+    print("Created users table in database")
+
+    db.execute("CREATE TABLE reviews ( id SERIAL PRIMARY KEY, review VARCHAR NOT NULL, rate INTEGER NOT NULL, user_id INTEGER REFERENCES users, book_id INTEGER REFERENCES books );")
+    print("Created reviews table in database")
+
+
     db.commit()
 
 if __name__ == "__main__":
